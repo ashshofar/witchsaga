2fdf0dbcf5ec9c4b3ac855bf108661f9f9a80a0e
inu <inu@voxteneo.com>
2021-04-17 13:45:15 +0700

    init commit

---
+++ Fibonacci.php
@@ -0,0 +1,102 @@
+<?php
+
+
+class Fibonacci
+{
+    /**
+     * @param $n
+     * @return int
+     */
+    public function recursive($n)
+    {
+        if ($n == 0) {
+            return 0;
+        }
+
+        if ($n == 1) {
+            return 1;
+        }
+
+        return $this->recursive($n - 1) + $this->recursive($n - 2);
+    }
+
+    /**
+     * @param $n
+     * @return array
+     */
+    public function calculate($n)
+    {
+        $temp = [];
+        for ($i=1; $i<=$n; $i++) {
+            array_push($temp, $this->recursive($i));
+        }
+
+        return $temp;
+    }
+
+    /**
+     * @param $post
+     * @return array
+     */
+    public function postData($post)
+    {
+        $validation = $this->validateData($post);
+
+        if (!$validation['status']) {
+            return $validation;
+        }
+
+        $person = $post['person'];
+
+        $result = [];
+        $tempAverage = [];
+        foreach ($person as $key=>$p) {
+            $born = $p['death'] - $p['age'];
+
+            $tempKilled = $this->calculate($born);
+            $killed = array_sum($tempKilled);
+
+            $resultPerson[$key]['age'] =  $p['age'];
+            $resultPerson[$key]['death'] = $p['death'];
+            $resultPerson[$key]['born'] = $born;
+            $resultPerson[$key]['tempKilled'] = implode(" + ", $tempKilled);
+            $resultPerson[$key]['killed'] = $killed;
+
+            array_push($tempAverage, $killed);
+        }
+
+        $sanitizeAverage = array_filter($tempAverage);
+        $result['person'] = $resultPerson;
+        $result['average'] = array_sum($sanitizeAverage) / count($sanitizeAverage);
+        $result['status'] = TRUE;
+
+        return $result;
+    }
+
+    /**
+     * @param $post
+     * @return array
+     */
+    public function validateData($post)
+    {
+        $person = $post['person'];
+        $result['status'] = TRUE;
+
+        // Check negative value
+        foreach ($person as $p) {
+            if ($p['death'] < 1 || $p['age'] < 1) {
+                $result['status'] = FALSE;
+                $result['message'] = 'Age of death or Year of death cannot be smaller than one';
+                $result['average'] = -1;
+            }
+
+            if ($p['death'] < $p['age'] || $p['death'] === $p['age']) {
+                $result['status'] = FALSE;
+                $result['message'] = 'Year of death cannot be smaller or equal than Age of death';
+                $result['average'] = -1;
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ index.php
@@ -0,0 +1,122 @@
+<?php
+    include 'Fibonacci.php';
+    $fibonacci = new Fibonacci();
+
+    $results = NULL;
+    if(isset($_POST['submit'])) {
+        $results = $fibonacci->postData($_POST);
+    }
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Witch Saga</title>
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Geekseat Witch Saga</h1>
+                    <p class="lead">Return of The Coder!</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    Input Data
+                </div>
+                <div class="card-body">
+                    <form action="index.php" method="POST">
+                        <h5> Person 1</h5>
+                        <div class="form-group row">
+                            <label for="colFormLabelLg" class="col-sm-2 col-form-label">Age of death</label>
+                            <div class="col-sm-10">
+                                <input type="number" name="person[0][age]" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelLg" class="col-sm-2 col-form-label">Year of death</label>
+                            <div class="col-sm-10">
+                                <input type="number" name="person[0][death]" class="form-control" required>
+                            </div>
+                        </div>
+                        <h5> Person 2</h5>
+                        <div class="form-group row">
+                            <label for="colFormLabelLg" class="col-sm-2 col-form-label">Age of death</label>
+                            <div class="col-sm-10">
+                                <input type="number" name="person[1][age]" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="colFormLabelLg" class="col-sm-2 col-form-label">Year of death</label>
+                            <div class="col-sm-10">
+                                <input type="number" name="person[1][death]" class="form-control" required>
+                            </div>
+                        </div>
+                        <hr>
+                        <input type="submit" name="submit" value="Submit" class="btn btn-primary float-right">
+                    </form>
+                </div>
+            </div>
+            <?php if(isset($_POST['submit'])) { ?>
+            <hr>
+            <div class="card">
+                <div class="card-header">
+                    Result
+                </div>
+                <div class="card-body">
+
+                    <?php if ($results['status']) { ?>
+
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>Person</th>
+                                    <th>Age of death</th>
+                                    <th>Year of death</th>
+                                    <th>Born on year</th>
+                                    <th>People killed on year</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($results['person'] as $key=>$result) { ?>
+
+                                    <tr>
+                                        <td>Person <?php echo $key+1 ?></td>
+                                        <td><?php echo $result['age'] ?></td>
+                                        <td><?php echo $result['death'] ?></td>
+                                        <td><?php echo $result['born'] ?></td>
+                                        <td><?php echo $result['tempKilled'] ?> = <?php echo $result['killed'] ?></td>
+                                    </tr>
+
+                                <?php } ?>
+                            </tbody>
+                        </table>
+
+                        <div class="alert alert-info" role="alert">
+                            <h4>The average is: <?php echo $results['average'] ?></h4>
+                        </div>
+                    </div>
+
+                    <?php } else { ?>
+                        <div class="alert alert-danger" role="alert">
+                            <h4><?php echo $results['message'] ?></h4>
+                            <h5>Result: <?php echo $results['average'] ?></h5>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </body>
+</html>
